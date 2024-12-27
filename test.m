c7564c8f3c68cf38c7e7e08a894814090927b42b
Katerina Papagiannitsi <aipapagi@ece.auth.gr>
2024-12-27 23:25:46 +0200

    Update distribution list to add more supported distributions

--- test.m
+++ test.m
@@ -2,8 +2,8 @@
 data = readtable('TMS.xlsx');
 
 % Extract relevant columns for ED duration with and without TMS
-ED_with_TMS = data.EDduration(data.TMS == 1);   % Replace with the actual column name
-ED_without_TMS = data.EDduration(data.TMS == 0); % Replace with the actual column name
+ED_with_TMS = data.EDduration(data.TMS == 1);
+ED_without_TMS = data.EDduration(data.TMS == 0);
 
 % Remove missing values
 ED_with_TMS = ED_with_TMS(~isnan(ED_with_TMS));
@@ -11,6 +11,8 @@ ED_without_TMS = ED_without_TMS(~isnan(ED_without_TMS));
 
 
 % List of distributions to test
-dist_names = {'Normal', 'Exponential', 'Lognormal', 'Weibull', 'Gamma'};
+dist_names = {'BirnbaumSaunders', 'Burr', 'Exponential', 'Extreme Value', 'Gamma', 'Generalized Extreme Value', 'Generalized Pareto', 'Half Normal', 'InverseGaussian', 'Kernel', 'Logistic', 'Loglogistic', 'Lognormal', 'Nakagami', 'Normal', 'Poisson', 'Rayleigh', 'Rician', 'tLocationScale', 'Weibull'};
+
+warning('off', 'all'); % Suppress warnings for better output readability
 % Perform goodness of fit test for ED duration with TMS
 test_goodness_of_fit(ED_with_TMS, dist_names);
