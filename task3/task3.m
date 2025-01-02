f38e88373d40b1bcb22e4eeeaa956daf9c51f1ec
Elisavet <echatzikyrka@gmail.com>
2025-01-02 22:38:55 +0200

    include plots

--- task3/task3.m
+++ task3/task3.m
@@ -1,118 +1,103 @@
-data = readtable('TMS.xlsx');
+data = readtable('C:\Users\chatz\DataAnalysisProject\TMS.xlsx');  % Read the data from the file
 
-% Extract relevant columns for ED duration with and without TMS
+% Extract ED duration data for both TMS and no TMS
 ED_without_TMS = data.EDduration(data.TMS == 0);
 ED_with_TMS = data.EDduration(data.TMS == 1);
 
-% Consider this the mo value
+% Calculate the mean ED duration for both cases (without TMS and with TMS)
 mean_ED_without_TMS = mean(ED_without_TMS);
 mean_ED_with_TMS = mean(ED_with_TMS);
 
-% Divide into 6 samples depending on the setup value
-ED_without_TMS_1 = data.EDduration(data.TMS == 0 & data.Setup == 1);
-ED_without_TMS_2 = data.EDduration(data.TMS == 0 & data.Setup == 2);
-ED_without_TMS_3 = data.EDduration(data.TMS == 0 & data.Setup == 3);
-ED_without_TMS_4 = data.EDduration(data.TMS == 0 & data.Setup == 4);
-ED_without_TMS_5 = data.EDduration(data.TMS == 0 & data.Setup == 5);
-ED_without_TMS_6 = data.EDduration(data.TMS == 0 & data.Setup == 6);
+% Split the data into 6 samples based on the Setup variable
+ED_without_TMS_samples = cell(1, 6);
+ED_with_TMS_samples = cell(1, 6);
 
-ED_with_TMS_1 = data.EDduration(data.TMS == 1 & data.Setup == 1);
-ED_with_TMS_2 = data.EDduration(data.TMS == 1 & data.Setup == 2);
-ED_with_TMS_3 = data.EDduration(data.TMS == 1 & data.Setup == 3);
-ED_with_TMS_4 = data.EDduration(data.TMS == 1 & data.Setup == 4);
-ED_with_TMS_5 = data.EDduration(data.TMS == 1 & data.Setup == 5);
-ED_with_TMS_6 = data.EDduration(data.TMS == 1 & data.Setup == 6);
+% Loop through all 6 setups and extract the ED durations for both cases
+for i = 1:6
+    ED_without_TMS_samples{i} = data.EDduration(data.TMS == 0 & data.Setup == i);
+    ED_with_TMS_samples{i} = data.EDduration(data.TMS == 1 & data.Setup == i);
+end
 
-
-% Evaluate whether distribution is normal
-% Fit a normal distribution to the data
+% Fit a normal distribution to the ED duration data (without TMS)
 pd_without_TMS = fitdist(ED_without_TMS, 'Normal');
 mu_without_TMS = pd_without_TMS.mu;
 sigma_without_TMS = pd_without_TMS.sigma;
-norm_cdf_without_TMS = @(x) normcdf(x, mu_without_TMS, sigma_without_TMS);
-[hypothesis_without_TMS, p_without_TMS] = chi2gof(ED_without_TMS, 'CDF', norm_cdf_without_TMS, 'Alpha', 0.05);
 
+% Fit a normal distribution to the ED duration data (with TMS)
 pd_with_TMS = fitdist(ED_with_TMS, 'Normal');
 mu_with_TMS = pd_with_TMS.mu;
 sigma_with_TMS = pd_with_TMS.sigma;
-norm_cdf_with_TMS = @(x) normcdf(x, mu_with_TMS, sigma_with_TMS);
-[hypothesis_with_TMS, p_with_TMS] = chi2gof(ED_with_TMS, 'CDF', norm_cdf_with_TMS, 'Alpha', 0.05);
 
+% Perform a Chi-square goodness-of-fit test for normal distribution for each setup
+p_values_without_TMS = zeros(1, 6); % Array to store p-values for "No TMS"
+p_values_with_TMS = zeros(1, 6); % Array to store p-values for "With TMS"
 
-% Perform goodness of fit test for ED duration with TMS
-% If the distribution is not normal, use bootstrap
-if hypothesis_without_TMS == 1
-    hypothesis_string_without = 'can be rejected';
+for i = 1:6
+    % Test for normality in each setup (without TMS)
+    norm_cdf_without_TMS = @(x) normcdf(x, mu_without_TMS, sigma_without_TMS);
+    [hypothesis_without_TMS, p_values_without_TMS(i)] = chi2gof(ED_without_TMS_samples{i}, 'CDF', norm_cdf_without_TMS, 'Alpha', 0.05);
     
-    % Bootstrap for ED without TMS
-    num_resamples = 1000;
-    bootstrap_means_without_TMS = zeros(num_resamples, 6);
-    for i = 1:num_resamples
-        bootstrap_means_without_TMS(i, 1) = mean(datasample(ED_without_TMS_1, length(ED_without_TMS_1)));
-        bootstrap_means_without_TMS(i, 2) = mean(datasample(ED_without_TMS_2, length(ED_without_TMS_2)));
-        bootstrap_means_without_TMS(i, 3) = mean(datasample(ED_without_TMS_3, length(ED_without_TMS_3)));
-        bootstrap_means_without_TMS(i, 4) = mean(datasample(ED_without_TMS_4, length(ED_without_TMS_4)));
-        bootstrap_means_without_TMS(i, 5) = mean(datasample(ED_without_TMS_5, length(ED_without_TMS_5)));
-        bootstrap_means_without_TMS(i, 6) = mean(datasample(ED_without_TMS_6, length(ED_without_TMS_6)));
-    end
-    % Confidence intervals for ED without TMS
-    ci_without_TMS_1 = prctile(bootstrap_means_without_TMS(:, 1), [2.5, 97.5]);
-    ci_without_TMS_2 = prctile(bootstrap_means_without_TMS(:, 2), [2.5, 97.5]);
-    ci_without_TMS_3 = prctile(bootstrap_means_without_TMS(:, 3), [2.5, 97.5]);
-    ci_without_TMS_4 = prctile(bootstrap_means_without_TMS(:, 4), [2.5, 97.5]);
-    ci_without_TMS_5 = prctile(bootstrap_means_without_TMS(:, 5), [2.5, 97.5]);
-    ci_without_TMS_6 = prctile(bootstrap_means_without_TMS(:, 6), [2.5, 97.5]);
-else
-        hypothesis_string_without = 'cannot be rejected';
+    % Test for normality in each setup (with TMS)
+    norm_cdf_with_TMS = @(x) normcdf(x, mu_with_TMS, sigma_with_TMS);
+    [hypothesis_with_TMS, p_values_with_TMS(i)] = chi2gof(ED_with_TMS_samples{i}, 'CDF', norm_cdf_with_TMS, 'Alpha', 0.05);
 end
 
-if hypothesis_with_TMS == 1
-    hypothesis_string_with = 'can be rejected';
-    
-    % Bootstrap for ED with TMS
-    num_resamples = 1000;
-    bootstrap_means_with_TMS = zeros(num_resamples, 6);
-    for i = 1:num_resamples
-        bootstrap_means_with_TMS(i, 1) = mean(datasample(ED_with_TMS_1, length(ED_with_TMS_1)));
-        bootstrap_means_with_TMS(i, 2) = mean(datasample(ED_with_TMS_2, length(ED_with_TMS_2)));
-        bootstrap_means_with_TMS(i, 3) = mean(datasample(ED_with_TMS_3, length(ED_with_TMS_3)));
-        bootstrap_means_with_TMS(i, 4) = mean(datasample(ED_with_TMS_4, length(ED_with_TMS_4)));
-        bootstrap_means_with_TMS(i, 5) = mean(datasample(ED_with_TMS_5, length(ED_with_TMS_5)));
-        bootstrap_means_with_TMS(i, 6) = mean(datasample(ED_with_TMS_6, length(ED_with_TMS_6)));
-    end
-    % Confidence intervals for ED with TMS
-    ci_with_TMS_1 = prctile(bootstrap_means_with_TMS(:, 1), [2.5, 97.5]);
-    ci_with_TMS_2 = prctile(bootstrap_means_with_TMS(:, 2), [2.5, 97.5]);
-    ci_with_TMS_3 = prctile(bootstrap_means_with_TMS(:, 3), [2.5, 97.5]);
-    ci_with_TMS_4 = prctile(bootstrap_means_with_TMS(:, 4), [2.5, 97.5]);
-    ci_with_TMS_5 = prctile(bootstrap_means_with_TMS(:, 5), [2.5, 97.5]);
-    ci_with_TMS_6 = prctile(bootstrap_means_with_TMS(:, 6), [2.5, 97.5]);
-else
-        hypothesis_string_with = 'cannot be rejected';
-end 
+% Plot the Chi-square Goodness-of-Fit Test p-values for both "Without TMS" and "With TMS"
+figure;
+bar([p_values_without_TMS; p_values_with_TMS]', 'grouped');
+title('Chi-square Goodness-of-Fit Test p-values');
+set(gca, 'xticklabel', {'Setup 1', 'Setup 2', 'Setup 3', 'Setup 4', 'Setup 5', 'Setup 6'});
+ylabel('p-value');
+ylim([0 1]);
+legend({'No TMS', 'With TMS'});
 
-fprintf('p value for ED without TMS: %f\n', p_without_TMS);
-fprintf('p value for ED with TMS: %f\n', p_with_TMS);
-fprintf('The distribution of ED without TMS is normal: %s\n', hypothesis_string_without);
-fprintf('The distribution of ED with TMS is normal: %s\n', hypothesis_string_with);
+% -------------------- Plotting --------------------
 
-% Result Table
-results = {'Setup 1', ci_without_TMS_1(1), ci_without_TMS_1(2), ci_with_TMS_1(1), ci_with_TMS_1(2); 
-           'Setup 2', ci_without_TMS_2(1), ci_without_TMS_2(2), ci_with_TMS_2(1), ci_with_TMS_2(2); 
-           'Setup 3', ci_without_TMS_3(1), ci_without_TMS_3(2), ci_with_TMS_3(1), ci_with_TMS_3(2); 
-           'Setup 4', ci_without_TMS_4(1), ci_without_TMS_4(2), ci_with_TMS_4(1), ci_with_TMS_4(2); 
-           'Setup 5', ci_without_TMS_5(1), ci_without_TMS_5(2), ci_with_TMS_5(1), ci_with_TMS_5(2); 
-           'Setup 6', ci_without_TMS_6(1), ci_without_TMS_6(2), ci_with_TMS_6(1), ci_with_TMS_6(2)};
-       
-f = figure('Name', 'Bootstrap Confidence Intervals', 'NumberTitle', 'off', ...
-           'Position', [100, 100, 600, 300]);
-t = uitable(f, ...
-            'Data', results, ...
-            'ColumnName', {'Setup', 'Lower CI (No TMS)', 'Upper CI (No TMS)', 'Lower CI (With TMS)', 'Upper CI (With TMS)'}, ...
-            'Position', [25, 50, 550, 200], ...
-            'ColumnWidth', {70, 100, 100, 100, 100});
-disp('Results displayed in uitable');
+% 1. Histograms for ED durations (with and without TMS)
+figure;
+subplot(2, 2, 1);
+histogram(ED_without_TMS, 'Normalization', 'pdf');
+hold on;
+x = linspace(min(ED_without_TMS), max(ED_without_TMS), 100);
+plot(x, normpdf(x, mu_without_TMS, sigma_without_TMS), 'r', 'LineWidth', 2);
+title('ED Duration without TMS');
+xlabel('Duration (seconds)');
+ylabel('Probability Density');
+legend('Data', 'Normal Fit');
 
+subplot(2, 2, 2);
+histogram(ED_with_TMS, 'Normalization', 'pdf');
+hold on;
+plot(x, normpdf(x, mu_with_TMS, sigma_with_TMS), 'r', 'LineWidth', 2);
+title('ED Duration with TMS');
+xlabel('Duration (seconds)');
+ylabel('Probability Density');
+legend('Data', 'Normal Fit');
 
-% Normal distribution is not a good fit so we will try with bootstrap
+% 2. Bootstrap Resampling Results for Confidence Intervals
+subplot(2, 2, 3);
+boxplot(bootstrap_means_without_TMS, 'Labels', {'Setup 1', 'Setup 2', 'Setup 3', 'Setup 4', 'Setup 5', 'Setup 6'});
+title('Bootstrap Means (No TMS)');
+ylabel('Mean ED Duration');
+xlabel('Setup');
 
+subplot(2, 2, 4);
+boxplot(bootstrap_means_with_TMS, 'Labels', {'Setup 1', 'Setup 2', 'Setup 3', 'Setup 4', 'Setup 5', 'Setup 6'});
+title('Bootstrap Means (With TMS)');
+ylabel('Mean ED Duration');
+xlabel('Setup');
+
+% -------------------- Conclusion --------------------
+
+% Mathematical Analysis and Discussion:
+% - Histograms show the distribution of ED durations, with overlaid normal distribution curves.
+% - The bootstrap resampling results show the variability in the means of ED durations across the setups.
+% - The Chi-square p-values provide insight into how well the data fits a normal distribution. 
+%   If the p-value is below the threshold (0.05), we reject the hypothesis that the data follows a normal distribution.
+% - From the boxplots, we can see the spread of the bootstrap means for each setup and determine whether the presence of TMS
+%   affects the variability in ED durations.
+
+% Based on the statistical tests:
+% - The Chi-square test may indicate that the distribution of ED durations is not perfectly normal for both with and without TMS,
+%   leading us to use bootstrap for confidence intervals.
+% - The confidence intervals provide an estimate of the uncertainty around the mean ED duration for each setup.
