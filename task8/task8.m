39f430ad120d09077331c5e36d8be12fdc230e33
Elisavet <echatzikyrka@gmail.com>
2025-01-14 21:58:29 +0200

    initial idea for task8, pcr remaining

--- task8/task8.m
+++ task8/task8.m
@@ -1,4 +1,6 @@
 % Group40Ex8
+lambda_task6 = 0.1302;
+warning('off', 'all');
 
 % Read the data from the file
 current_file_path = mfilename('fullpath');
@@ -28,21 +30,53 @@ for i = 1:length(columns_to_convert)
 end
 
 % Select variables
-indepedent_vars = table2array(data_with_TMS_Spike(:, {'Setup', 'Stimuli', 'Intensity', 'Spike', 'Frequency', 'CoilCode', 'preTMS'}));
+indepedent_vars = table2array(data_with_TMS_Spike(:, {'Setup', 'Stimuli', 'Intensity', 'Frequency', 'CoilCode', 'preTMS'}));
+EDduration = data_with_TMS_NoSpike.EDduration;
+
+% Normalize the data
+mx = mean(indepedent_vars);
+X = indepedent_vars - mx;
+y = EDduration - mean(EDduration);
 
 %% Full model
 % We chose to remove the Spike variable (task 6)
 data_with_TMS_noSpike = removevars(data_with_TMS_Spike, 'Spike');
-X = indepedent_vars;
-y = data_with_TMS_noSpike.EDduration;
 
 % Fit the model
-lm_full_no_spike = fitlm(X, y);
-mse_full_no_spike = lm_full_no_spike.MSE;
+lm_full = fitlm(X, y);
+mse_full = lm_full_no_spike.MSE;
 r2_full_no_spike = lm_full_no_spike.Rsquared.Adjusted;
 
 %% Stepwise model
-[b,~,stats] = stepwisefit(X, y, 'display', 'off');
+lm_stepwise = stepwisefit(X, y, 'Verbose', 0);
+Y_pred_stepwise= predict(lm_stepwise_train, X);
+mse_stepwise = mean((y - Y_pred_stepwise).^2);
+
+%% LASSO
+[beta, fitinfo] = lasso(X, y, 'CV', 10);
+lambda_optimal = lambda_task6;
+[~, ilmin] = min(abs(fitinfo.Lambda - lambda_optimal));
+bLASSOV = beta(:, ilmin);
+mxV = mean(X);
+my = mean(y);
+bLASSOV = [my - mxV * bLASSOV; bLASSOV];
+yfitLASSOV = [ones(length(y), 1) X] * bLASSOV;
+mse_lasso = mean((y - yfitLASSOV).^2);
+
+
+%% PCR
 
 
 
+
+
+% Μένει να γινει το pcr 
+% Πόσες συνιστώσες να επιλεχθούν?
+% Να συγκριθουν τα μοντελα με το pcr 
+% Να συγκριθουν με το linear μοντελο της ασκησης 5
+
+% Να ξαναλυθει η ασκηση αλλά
+% Να προσθεσουμε σαν ανεξαρτητη μεταβλητη το ΚΑΙ postTMS
+
+
+% Να διορθώσουμε τον κώδικα για το task 6
